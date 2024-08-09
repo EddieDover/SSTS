c1d5fe250188bdc5b76a39025436022d9bb5c0dc
Alex Irwin <alex07.irwin@btinternet.com>
2024-08-09 14:24:37 +0100

    added functions for checking requirements

--- backend/src/application/functions.py
+++ backend/src/application/functions.py
@@ -1,58 +1,68 @@
-db=[]
-#db is the database and is not already configured
-goals=[]
-subjects=[]
-grades=[]
+
+#variables: name, durname, id, email, password, confirm_pass, valid(boolean),  
 
 def check_email(email):
     valid=False
-    at_symbol=False
-    dot=False
     for i in range (0,len(email)):
         if email[i]=="@":
-            at_symbol=True
-        if email[i]==".":
-            dot=True
-    if at_symbol==True and dot==True:
-        valid=True
-
-
-def register_user(name,surname,id,email,password,confirm_pass):
-    details=[]
-    present=False
-    for i in range(0,len(db)):
-        if id==db[0][i]:
-            #user already in db
-            present=True
-    if present==False:
-        check_email(email)
+            for j in range (i,len(email)):
+                if email[i]==".":
+                    valid=True      
         if valid==True:
-            details.ammend(name,surname,id,email,password,confirm_pass)
-            db.ammend(details)
-        
-def check_login(email,password):
-    login=False
-    for i in range (0,len(db)):
-        if email==db[1][i]:
-            if password==db[2][i]:
-                login=True
-            #password doesnt match with email
-        #email is not in database
+            break
 
-def add_goal(goal):
-    goals.ammend(goal)
-    #adds goal to goals list
+def check_password_symbol(password):
+    valid=False
+    for i in range (0, len(password)):
+        for symbol in range(33,48):
+            if password[i]==chr(symbol):
+                valid=True
+        for symbol in range(58,65):
+            if password[i]==chr(symbol):
+                valid=True
+        for symbol in range(91,97):
+            if password[i]==chr(symbol):
+                valid=True
+        for isymbol in range(123,127):
+            if password[i]==chr(symbol):
+                valid=True
+#checks if there is a symbol
 
-def add_subject(subject):
-    subjects.ammend(subject)
-    #adds subject to subjects list
+def check_password_number(password):
+    valid=False
+    for i in range (0,len(password)):
+        for number in range(0,10):
+            if password[i]==str(number):
+                valid=True
+#checks if there is a number
 
-def add_grade(grade,date):
-    details=[]
-    details.ammend(grade,date)
-    grades.ammend(details)
-    #adds grade and date of grade to grades list
+def check_confirm_password(password,confirm_pass):
+    valid_pass=False
+    if password>=8:
+        check_password_number(password)
+        check_password_symbol(password)
+        if password==confirm_pass:
+            valid_pass=True
+#checks teh length and if is same as confirm
 
-
-
+def valid_name_surname(name, surname):
+    valid_name=True
+    temp_name=""
+    for different_names in range (0,2):
+        if different_names==0:
+            temp_name=name
+        if different_names==1:
+            temp_name=surname
+        #changes between surname and name so that they are both checked
+        for i in range (0,len(temp_name)):
+            for invalid in range(33,65):
+                if temp_name[i]==chr(invalid):
+                    valid_name=False
+                    #checks if there are any numbers in the name
+            for invalid in range(91,97):
+                if temp_name[i]==chr(invalid):
+                    valid_name=False
+            for invalid in range(123,127):
+                if temp_name[i]==chr(invalid):
+                    valid_name=False
 
